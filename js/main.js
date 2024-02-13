3fc7d47b8e8c02b20f8720234279e8291bbb6059
Maxim <115576894+GitMaxm@users.noreply.github.com>
2024-02-13 23:28:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/* ---------------- mobile nav -------------------------- */
+
+function mobileNav() {
+    // Mobile nav button
+    const navBtn = document.querySelector('.mobile-nav-btn');
+    const nav = document.querySelector('.mobile-nav');
+    const menuIcon = document.querySelector('.nav-icon');
+
+    // Функция для закрытия меню
+    function closeNav() {
+        nav.classList.remove('mobile-nav--open');
+        menuIcon.classList.remove('nav-icon--active');
+        document.body.classList.remove('no-scroll');
+    }
+
+    navBtn.onclick = function () {
+        nav.classList.toggle('mobile-nav--open');
+        menuIcon.classList.toggle('nav-icon--active');
+        document.body.classList.toggle('no-scroll');
+    };
+
+    // Получаем все ссылки внутри меню
+    const navLinks = document.querySelectorAll('.mobile-nav a');
+
+    // Добавляем обработчик события для каждой ссылки
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            closeNav(); // Закрываем меню при клике на ссылку
+        });
+    });
+}
+
+mobileNav();
+
+
+/* ----------------------- Swiper -----------------------------*/
+
+function swiperFunc() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 3,
+        grid: {
+            rows: 2,
+        },
+        spaceBetween: 26,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.button-next',
+            prevEl: '.button-prev',
+        },
+        breakpoints: {
+            1420: {
+                slidesPerView: 2,
+                spaceBetween: 21,
+            },
+        }
+    });
+}
+
+swiperFunc()
